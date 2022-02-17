afb984f17be2698fa2c732b65a703f32ef5667e3
Stanley Varga <stanislav.varga@brackets.sk>
2022-02-17 20:20:02 +0100

    max profit

--- src/leet-code/max-profit/index.test.js
+++ src/leet-code/max-profit/index.test.js
@@ -3,14 +3,22 @@ import { maxProfit } from "./max-profit";
 describe("maxProfit", () => {
   it("should find a minimum number and return zero", () => {
     expect(maxProfit([9, 8, 7, 6, 5, 1])).toEqual(0);
+    expect(maxProfit([1])).toBe(0);
+    expect(maxProfit([2, 1])).toBe(0);
   });
   it("should find a profit value", () => {
-    expect(maxProfit([9, 8, 7, 6, 5, 1, 5, 5, 2, 3, 8])).toEqual(7);
-    expect(maxProfit([7, 6, 4, 3, 1])).toEqual(0);
-    expect(maxProfit([7, 1, 5, 3, 6, 4])).toEqual(5);
-
-    // Explaination:
-    // buying for two, selling for 4
     expect(maxProfit([2, 4, 1])).toEqual(2);
+  });
+
+  it("should find max profit when low is first", () => {
+    expect(maxProfit([1, 2, 3, 4, 5, 6, 7])).toBe(6);
+  });
+
+  it("should return max profit when low is middle", () => {
+    expect(maxProfit([5, 6, 7, 1, 2, 3, 4])).toBe(3);
+  });
+
+  it("should return max profit when low is before last", () => {
+    // expect(maxProfit([3, 2, 6, 5, 0, 3])).toBe(3);
   });
 });
--- src/leet-code/max-profit/max-profit.ts
+++ src/leet-code/max-profit/max-profit.ts
@@ -1,24 +1,34 @@
-// TODO TOMORROW - M*n, for each price, compare maximum profit
-// with every other price
 export function maxProfit(prices: number[]): number {
-  let min = Number.MAX_SAFE_INTEGER;
-  let max = -1;
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      let profit = prices[j] - prices[i];
+      if (profit > maxProfit) {
+        maxProfit = profit;
+      }
+    }
+  }
+  return maxProfit;
+}
+
+export function maxProfitWrongSolution(prices: number[]): number {
   let i = 0;
-  let j = 0;
+  let low = prices[i];
+  let high = 0;
 
-  for (; i < prices.length; i++) {
-    if (prices[i] < min) {
-      min = prices[i];
-      j = i;
+  for (i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[j] < low) {
+        i = j;
+        low = prices[i];
+        high = 0;
+      }
+
+      if (high < prices[j]) {
+        high = prices[j];
+      }
     }
   }
 
-  for (j = j + 1; j < prices.length; j++) {
-    if (max < prices[j]) {
-      max = prices[j];
-    }
-  }
-
-  max < min && (max = min);
-  return max - min;
+  return high <= 0 || low > high ? 0 : high - low;
 }
