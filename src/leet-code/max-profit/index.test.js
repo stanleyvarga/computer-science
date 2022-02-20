925ba2b00e62f356da3bd46b37f279187fc872d4
Stanley Varga <stanislav.varga@brackets.sk>
2022-02-20 17:48:03 +0100

    max-profit WIP

--- src/leet-code/max-profit/index.test.js
+++ src/leet-code/max-profit/index.test.js
@@ -1,24 +1,23 @@
-import { maxProfit } from "./max-profit";
+import { maxProfit_N2, maxProfit_N } from "./max-profit";
 
 describe("maxProfit", () => {
   it("should find a minimum number and return zero", () => {
-    expect(maxProfit([9, 8, 7, 6, 5, 1])).toEqual(0);
-    expect(maxProfit([1])).toBe(0);
-    expect(maxProfit([2, 1])).toBe(0);
-  });
-  it("should find a profit value", () => {
-    expect(maxProfit([2, 4, 1])).toEqual(2);
-  });
+    // expect(maxProfit_N2([9, 8, 7, 6, 5, 1])).toEqual(0);
+    // expect(maxProfit_N2([1])).toBe(0);
+    // expect(maxProfit_N2([2, 1])).toBe(0);
+    // expect(maxProfit_N2([3, 2, 6, 5, 0, 3])).toBe(3);
+    // expect(maxProfit_N2([2, 4, 1])).toEqual(2);
+    // expect(maxProfit_N2([5, 6, 7, 1, 2, 3, 4])).toBe(3);
+    // expect(maxProfit_N2([1, 2, 3, 4, 5, 6, 7])).toBe(6);
+    // expect(maxProfit_N2([9, 8, 7, 6, 5, 1])).toEqual(0);
 
-  it("should find max profit when low is first", () => {
-    expect(maxProfit([1, 2, 3, 4, 5, 6, 7])).toBe(6);
-  });
-
-  it("should return max profit when low is middle", () => {
-    expect(maxProfit([5, 6, 7, 1, 2, 3, 4])).toBe(3);
-  });
-
-  it("should return max profit when low is before last", () => {
-    // expect(maxProfit([3, 2, 6, 5, 0, 3])).toBe(3);
+    expect(maxProfit_N([9, 8, 7, 6, 5, 1])).toEqual(0);
+    expect(maxProfit_N([1])).toBe(0);
+    expect(maxProfit_N([2, 1])).toBe(0);
+    expect(maxProfit_N([3, 2, 6, 5, 0, 3])).toBe(3);
+    expect(maxProfit_N([2, 4, 1])).toEqual(2);
+    expect(maxProfit_N([2, 4, 3, 2, 1])).toEqual(2);
+    expect(maxProfit_N([5, 6, 7, 1, 2, 3, 4])).toBe(3);
+    expect(maxProfit_N([1, 2, 3, 4, 5, 6, 7])).toBe(6);
   });
 });
